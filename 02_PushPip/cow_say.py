77944bdb54473206e9c6b6b9092fe5b2479cef64
Masha <masha.burdugova@mail.ru>
2024-02-25 12:57:03 +0300

    addet output

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,12 +1,15 @@
 import argparse
-from cowsay import cowsay
-from cowsay import list_cows
+import cowsay
+import sys
+
 
 
 parser = argparse.ArgumentParser(prog='cow_say', description='Set the parameters')
 
+parser.add_argument("message", nargs='*', default=[' '])
+
 parser.add_argument('-n', action='store_false')
-parser.add_argument('-W', type = int, default = 40)
+parser.add_argument('-W', default=40)
 
 parser.add_argument('-b', action='store_true')
 parser.add_argument('-d', action='store_true')
@@ -16,13 +19,21 @@ parser.add_argument('-t', action='store_true')
 parser.add_argument('-w', action='store_true')
 parser.add_argument('-y', action='store_true')
 
-parser.add_argument('-e', dest='eye_string', default = "oo")
-parser.add_argument('-T', dest='tongue_string', default = "")
+parser.add_argument('-e', dest='eye_string', default="oo")
+parser.add_argument('-T', dest='tongue_string', default="")
 parser.add_argument('-f', dest='cowfile')
 parser.add_argument('-l', action='store_true')
 
+parser.add_argument("-bdgptwye", dest="preset")
+
+args = parser.parse_args()
 
 
-print(cowsay("Ha-ha-not-so-much"))
+if args.l:
+    print(cowsay.list_cows())
+else:
+    cows = cowsay.cowsay(args.message, preset=args.preset, eyes=args.e, tongue=args.T, width=args.W, wrap_text=args.n, cowfile=args.f)
+    print(cows)
 
 
+print(cow("Ha-ha-not-so-much"))
