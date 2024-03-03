b77847f83c4e05353d4d7c822eaf065f9532c464
Maria <masha.burdugova@mail.ru>
2024-03-03 20:32:26 +0000

    added gameplay

--- 03_MergeRequirements/bullcows.py
+++ 03_MergeRequirements/bullcows.py
@@ -12,5 +12,14 @@ def bullscows(guess: str, secret: str) -> (int, int):
             cows += 1
     return (bulls, cows)
 
-
-
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    attempt = 1
+    word = random.choice(words)
+    try_word = ask("Введите слово: ", words)
+    while try_word != word:
+        bulls, cows = bullscows(try_word, word)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+        attempt += 1
+        try_word = ask("Введите слово: ", words)
+    if try_word.lower() == word.lower():
+        return attempt
